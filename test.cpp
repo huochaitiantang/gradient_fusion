97a5b332358cf22ccabe58571a8aeda0eaf06b8f
liuliang <1432249204@qq.com>
2017-07-17 20:13:52 +0800

    premary but slow

--- test.cpp
+++ test.cpp
@@ -13,146 +13,124 @@ int print_mat_info(Mat mat){
 	return 0;
 }
 
-void computeGradientX( const Mat &img, Mat &gx){  
-	Mat kernel = Mat::zeros(1, 3, CV_8S);  
-	kernel.at<char>(0,2) = 1;  
-	kernel.at<char>(0,1) = -1;  
-	      
-	if(img.channels() == 3){  
-		filter2D(img, gx, CV_32F, kernel);  
-	}  
-	else if (img.channels() == 1){  
-		Mat tmp[3];  
-		for(int chan = 0 ; chan < 3 ; ++chan){
-			filter2D(img, tmp[chan], CV_32F, kernel);
-		}
-		merge(tmp, 3, gx); 
-	}  
-}  
-  
-void computeGradientY( const Mat &img, Mat &gy){  
-	Mat kernel = Mat::zeros(3, 1, CV_8S);  
-	kernel.at<char>(2,0) = 1;  
-	kernel.at<char>(1,0) = -1;  	
-	if(img.channels() == 3)  {  
-		filter2D(img, gy, CV_32F, kernel); 		
-       	}  
-	else if (img.channels() == 1)  {  
-		Mat tmp[3];  
-		for(int chan = 0 ; chan < 3 ; ++chan)  {  
-			filter2D(img, tmp[chan], CV_32F, kernel);  
-		}  
-		merge(tmp, 3, gy);  				
-	}  
-} 
-
-void compute_laplacian(const Mat &img, Mat &ans){
-	Mat lap_x, lap_y;
-	
-	Mat kernel_x = Mat::zeros(1, 3, CV_8S);  
-	kernel_x.at<char>(0,0) = -1;  
-	kernel_x.at<char>(0,1) = 1;  
-	filter2D(img, lap_x, CV_32F, kernel_x);  
-	
-	Mat kernel_y = Mat::zeros(1, 3, CV_8S);  
-	kernel_y.at<char>(0,0) = -1;  
-	kernel_y.at<char>(0,1) = 1;  
-	filter2D(img, lap_y, CV_32F, kernel_y);
-
-	ans = lap_x + lap_y;	
-}	
-
-void dst(const Mat& src, Mat& dest, bool invert){  
-	Mat temp = Mat::zeros(src.rows, 2 * src.cols + 2, CV_32F);  
-	int flag = invert ? DFT_ROWS + DFT_SCALE + DFT_INVERSE: DFT_ROWS;  
-	src.copyTo(temp(Rect(1,0, src.cols, src.rows)));  	
-	for(int j = 0 ; j < src.rows ; ++j){  
-		float * tempLinePtr = temp.ptr<float>(j);  
-		const float * srcLinePtr = src.ptr<float>(j);  
-		for(int i = 0 ; i < src.cols ; ++i){  	        			
-			tempLinePtr[src.cols + 2 + i] = - srcLinePtr[src.cols - 1 - i];  
-		}  
-	}  		  
-	Mat planes[] = {temp, Mat::zeros(temp.size(), CV_32F)};  
-	Mat complex;  			  
-	merge(planes, 2, complex);  
-	dft(complex, complex, flag);  
-	split(complex, planes);  
-	temp = Mat::zeros(src.cols, 2 * src.rows + 2, CV_32F);  				  
-	for(int j = 0 ; j < src.cols ; ++j){  
-		float * tempLinePtr = temp.ptr<float>(j);  
-		for(int i = 0 ; i < src.rows ; ++i){  
-			float val = planes[1].ptr<float>(i)[j + 1];  
-			tempLinePtr[i + 1] = val;  
-			tempLinePtr[temp.cols - 1 - i] = - val;  
-		}  
-	}  
-
-	Mat planes2[] = {temp, Mat::zeros(temp.size(), CV_32F)};  
-
-	merge(planes2, 2, complex);  
-	dft(complex, complex, flag);  
-	split(complex, planes2);  
-
-	temp = planes2[1].t();  
-	dest = Mat::zeros(src.size(), CV_32F);  
-	temp(Rect( 0, 1, src.cols, src.rows)).copyTo(dest);  
-}  
-  
-void idst(const Mat& src, Mat& dest){  
-	dst(src, dest, true);  
+int get_ind(int i, int j, int w){
+	return i * w + j;
 }
 
-void solve(const Mat &img, Mat& mod_diff, Mat &result) {  
-	const int w = img.cols;  
-	const int h = img.rows;  
+void  getA(Mat &A, int h, int w){
+	Mat M, temp, roimat;
+	A = Mat::eye( h * w, h * w, CV_64FC1);
+	A *= -4;
+	M = Mat::zeros( h, w, CV_64FC1);
+
+	temp = Mat::ones( h, w - 2, CV_64FC1);
+	roimat = M( Rect( 1, 0, w - 2, h) );
+	temp.copyTo(roimat);
+
+	temp = Mat::ones( h - 2, w, CV_64FC1);
+	roimat = M( Rect( 0, 1, w, h - 2) );
+	temp.copyTo(roimat);
 	
-	Mat res;  
-	dst(mod_diff, res, true); 
+	temp = Mat::ones( h - 2, w - 2, CV_64FC1);
+	temp *= 2;
+	roimat = M( Rect( 1, 1, w - 2, h - 2) );
+	temp.copyTo(roimat);
 
-	for(int j = 0 ; j < h-2; j++){  
-		float * resLinePtr = res.ptr<float>(j);  
-		for(int i = 0 ; i < w-2; i++){  
-			resLinePtr[i] /= (filter_X[i] + filter_Y[j] - 4);  
-		}  
-	}  
+	cout << "start A" << endl;
+	for(int i = 0; i < h; i++){
+		for(int j = 0; j < w; j++){
+			int label = get_ind(i, j, w);
+			if( M.at<double>(i, j) == 0){
+				if( i == 0 ) A.at<double>( get_ind( i + 1, j, w ), label ) = 1;
+				else if( i == h - 1 ) A.at<double>( get_ind( i - 1, j, w ), label ) = 1;
+				if( j == 0 ) A.at<double>( get_ind( i, j + 1, w), label ) = 1;
+				else if( j = w - 1 ) A.at<double>( get_ind( i, j - 1, w), label ) = 1;
+			}
+			else if( M.at<double>(i, j) == 1){
+				if( i == 0 ){
+					A.at<double>( get_ind( i + 1, j, w ), label ) = 1;
+					A.at<double>( get_ind( i, j - 1, w ), label ) = 1;
+					A.at<double>( get_ind( i, j + 1, w ), label ) = 1;
+				}
+				else if( i == h - 1 ){
+					A.at<double>( get_ind( i - 1, j, w ), label ) = 1;
+					A.at<double>( get_ind( i, j - 1, w ), label ) = 1;
+					A.at<double>( get_ind( i, j + 1, w ), label ) = 1;
+				}
+				if( j == 0 ){
+					A.at<double>( get_ind( i, j + 1, w ), label ) = 1;
+					A.at<double>( get_ind( i - 1, j, w ), label ) = 1;
+					A.at<double>( get_ind( i + 1, j, w ), label ) = 1;
+				}
+				else if( j == w - 1 ){
+					A.at<double>( get_ind( i, j - 1, w ), label ) = 1;
+					A.at<double>( get_ind( i - 1, j, w ), label ) = 1;
+					A.at<double>( get_ind( i + 1, j, w ), label ) = 1;
+				}
+			}
+			else{
+				A.at<double>( get_ind( i, j - 1, w ), label ) = 1;
+				A.at<double>( get_ind( i, j + 1, w ), label ) = 1;
+				A.at<double>( get_ind( i - 1, j, w ), label ) = 1;
+				A.at<double>( get_ind( i + 1, j, w ), label ) = 1;
+			}
+		}
+	}
+	cout << "end A" << endl;
+}
 
-	idst(res, mod_diff);  
 
-	unsigned char *  resLinePtr = result.ptr<unsigned char>(0);  
-	const unsigned char * imgLinePtr = img.ptr<unsigned char>(0); 
-	const float * interpLinePtr = NULL;  
+void getB( Mat &img_front, Mat &img_back, Rect roi, Point pt, Mat &B){
+	Mat Lap;
+	int rh = roi.height;
+	int rw = roi.width;
+	Mat lk = Mat::zeros( 3, 3, CV_64FC1 );
+	lk.at<double>( 0, 1 ) = 1.0;
+	lk.at<double>( 1, 0 ) = 1.0;
+	lk.at<double>( 1, 2 ) = 1.0;
+	lk.at<double>( 2, 1 ) = 1.0;
+	lk.at<double>( 1, 1 ) = -4.0;
 
-	//first col  
-	for(int i = 0 ; i < w ; ++i)  
-		result.ptr<unsigned char>(0)[i] = img.ptr<unsigned char>(0)[i];  
+	B = Mat::zeros( rh * rw, 1, CV_64FC1);
+	cout << "start B" << endl;
+	filter2D( img_front, Lap, -1, lk);
+	for(int i = 0; i < rh; i++){
+		for( int j = 0; j < rw; j++){
+			double tmp = 0.0;
+			tmp += Lap.at<double>( (i + roi.y ), ( j + roi.x ) );
+			if( i == 0 ) tmp -= img_back.at<double>( i - 1 + pt.y, j + pt.x );
+			else if( i == rh - 1) tmp -= img_back.at<double>( i + 1 + pt.y, j + pt.x );
+			if( j == 0 ) tmp -= img_back.at<double>( i + pt.y, j - 1 + pt.x );
+			else if( j == rw - 1) tmp -= img_back.at<double>( i + pt.y, j + 1 + pt.x );
+			B.at<double>( get_ind( i, j, rw ), 0) = tmp;
+		}
+	}
+	cout << "end B" << endl;
+}
 
-	for(int j = 1 ; j < h-1 ; ++j)  { 
-		resLinePtr = result.ptr<unsigned char>(j);  
-		imgLinePtr  = img.ptr<unsigned char>(j);  
-		interpLinePtr = mod_diff.ptr<float>(j-1);  
+Mat get_result(Mat &A, Mat &B, int h){
+	Mat ans;
+	cout << "start solve" << endl;
+	solve(A, B, ans);
+	cout << "end solve" << endl;
+	ans = ans.reshape(0, h);
+	return ans;
+}
 
-		//first row  
-		resLinePtr[0] = imgLinePtr[0];  
-
-		for(int i = 1 ; i < w-1 ; ++i)  {  
-			float value = interpLinePtr[i-1];  
-			if(value < 0.)  
-				resLinePtr[i] = 0;  
-			else if (value > 255.0)  
-				resLinePtr[i] = 255;  
-			else  
-				resLinePtr[i] = static_cast<unsigned char>(value);  
-		}  
-		//last row  
-		resLinePtr[w-1] = imgLinePtr[w-1];  
-	}  
-	//last col  
-	resLinePtr = result.ptr<unsigned char>(h-1);  
-	imgLinePtr = img.ptr<unsigned char>(h-1);  
-	for(int i = 0 ; i < w ; ++i)
-		resLinePtr[i] = imgLinePtr[i];  
-}  
+void poisson(Mat &img_front, Mat &img_back, Rect roi, Point pt, Mat &ans){
+	int rh = roi.height;
+	int rw = roi.width;
+	Mat A, B;
+	getA( A, rh, rw);
+	vector <Mat> rgb_f,rgb_b,result;
+	split(img_front, rgb_f);
+	split(img_back, rgb_b);
+	for(int k = 0; k < 3; k++){
+		getB(rgb_f[k], rgb_b[k], roi, pt, B);	
+		result.push_back( get_result( A, B, rh ) );
+	}
+	merge( result, ans );
+}
 
 void compute_gradient( const Mat &img, Mat &ans){
 	int scale = 1;
@@ -160,13 +138,11 @@ void compute_gradient( const Mat &img, Mat &ans){
 	int ddepth = CV_16S;
 	Mat Ans_gray, grad, grad_x, grad_y, abs_grad_x, abs_grad_y;
 	GaussianBlur( img, img, Size(3,3), 0, 0, BORDER_DEFAULT );
-	//cvtColor( Ans, Ans_gray, CV_RGB2GRAY );
+	//cvtColor( img, Ans_gray, CV_RGB2GRAY );
 	Sobel( img, grad_x, ddepth, 1, 0, 3, scale, delta, BORDER_DEFAULT );
 	Sobel( img, grad_y, ddepth, 0, 1, 3, scale, delta, BORDER_DEFAULT );
-	//Scharr( Ans, grad_x, ddepth, 1, 0, scale, delta, BORDER_DEFAULT );
-	//Scharr( Ans, grad_y, ddepth, 0, 1, scale, delta, BORDER_DEFAULT );
-	//computeGradientX(Ans, grad_x);
-	//computeGradientY(Ans, grad_y);
+	//Scharr( img, grad_x, ddepth, 1, 0, scale, delta, BORDER_DEFAULT );
+	//Scharr( img, grad_y, ddepth, 0, 1, scale, delta, BORDER_DEFAULT );
 	convertScaleAbs( grad_x, abs_grad_x );
 	convertScaleAbs( grad_y, abs_grad_y );
 	addWeighted( abs_grad_x, 0.5, abs_grad_y, 0.5, 0, ans );
@@ -216,8 +192,8 @@ void simple_replace(const Mat& obj, int x, int y, const Mat &src, int sx, int sy
 int main (int argc, char **argv)
 {
 	Mat img1,img2;
-	img1 = imread("3.jpg", CV_LOAD_IMAGE_COLOR );
-	img2 = imread("5.jpeg", CV_LOAD_IMAGE_COLOR );
+	img1 = imread("fg.jpg", CV_LOAD_IMAGE_COLOR );
+	img2 = imread("fg.jpg", CV_LOAD_IMAGE_COLOR );
 	
 	if (!img1.data || !img2.data) {
 		cout << "No image data\n";
@@ -227,15 +203,15 @@ int main (int argc, char **argv)
 	print_mat_info(img1);
 	print_mat_info(img2);
 
-	int x = 324;
-	int y = 420;
-	int sx = 0;
-	int sy = 55;
-	int w = img2.cols;
-	int h = img2.rows - 55 - 30;
+	int x = 97;
+	int y = 257;
+	int sx = 25;
+	int sy = 260;
+	int w = 30;
+	int h = 45;
 	Mat Ans;
         simple_replace(img1, x, y, img2, sx, sy, w, h, Ans);
-	imwrite("3_5_simple_fusion.jpg",Ans);
+	imwrite("simple_fusion.jpg",Ans);
 	imshow("simple_fusion", Ans);
 	//Mat grad;
 	//compute_gradient(Ans,grad);
@@ -249,11 +225,13 @@ int main (int argc, char **argv)
 	//imshow("grad_f",grad_f);
 	simple_replace(grad_b, x, y, grad_f, sx, sy, w, h, grad_fusion);
 	print_mat_info(grad_fusion);
-	imshow("grad_fusion",grad_fusion);
+	//imshow("grad_fusion",grad_fusion);
 
-	compute_laplacian(grad_fusion, grad_lap);
+	//compute_laplacian(grad_fusion, grad_lap);
+	//Laplacian(grad_fusion, grad_lap,grad_fusion.depth());
 	//print_mat_info(grad_lap);
 	//imshow("grad_lap", grad_lap);
+	/*
 	vector<Mat> img_chan; 
 	vector<Mat> lap_chan; 
 	
@@ -267,7 +245,21 @@ int main (int argc, char **argv)
 	Mat result;
 	merge(img_chan, result);
 	imshow("result",result);	
+	*/
+	//Mat A;
+	//getA( A, 20, 30);
+	Mat res,in1, in2;
+	img1.convertTo( in1, CV_64FC3 );
+	img2.convertTo( in2, CV_64FC3 );
+	poisson(in2, in1, Rect(sx, sy, w, h), Point(x, y), res);
+	res.convertTo(res, CV_8UC1);
+	Mat roimat = img1( Rect( x, y, w, h ) );
+	res.copyTo(roimat);
 
+	//imshow("roi",res);
+	imshow("res",img1);
+	imwrite("poisson.jpg",img1);
+		
 	waitKey(0);
 	return 0;
 }
